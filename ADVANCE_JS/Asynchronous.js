106b3440f657f6e28ed7978404b1c536f7b2d1dd
kopal <kopalgangrade0@gmail.com>
2023-10-16 17:06:10 +0530

    Commit

---
+++ ADVANCE_JS/Asynchronous.js
@@ -0,0 +1,15 @@
+
+
+//  Aysncronous Javascript
+
+console.log('Hello');
+
+console.log('JavaScript');
+
+setTimeout(function(){
+    console.log('I am from setTimeout...');
+},3000);
+
+console.log('JavaScript');
+
+
---
+++ OBJECT/js_object.js
@@ -0,0 +1,35 @@
+
+// Primitive value => is a value that has no properties or methods. \
+
+// Primitive values are immutable
+
+// Creating a JavaScript Object
+
+// With JavaScript, you can define and create your own objects.
+
+// There are different ways to create new objects:
+
+// Create a single object, using an object literal.
+// Create a single object, with the keyword new.
+// Define an object constructor, and then create objects of the constructed type.
+// Create an object using Object.create().
+
+
+// Create a single object, using an object literal.
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+
+// Create a single object, with the keyword new.
+
+// The following example create a new JavaScript object using new Object(), and then adds 4 properties:
+
+
+// Define an object constructor, and then create objects of the constructed type.
+// Create an object using Object.create().
